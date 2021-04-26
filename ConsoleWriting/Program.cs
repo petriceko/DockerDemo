ac97dea506c9dce538fddac1518874c8dc98abe1
Peter Ovsak <povsak@gmail.com>
2021-04-26 23:36:13 +0200

    Changes

--- ConsoleWriting/Program.cs
+++ ConsoleWriting/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleWriting
 
             while (true)
             {
-                Console.WriteLine($"Hello World! - {count}");
+                Console.WriteLine($"Hello World! - {count} - Update2");
                 await File.AppendAllTextAsync("WriteLines.txt", "Hello World!" + Environment.NewLine);
                 await Task.Delay(1000);
                 count++;
